dbd285a31f95b7e80caddc509225849366cb10f4
LUIS1508-EC <151102084+LUIS1508-EC@users.noreply.github.com>
2024-03-17 11:54:24 -0600

    ,

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,21 @@
+/*=======================================================================
+  ======================== FUNCION PINTAR.HTML ==========================
+  =======================================================================*/
+
 const pintar = (element,color) => element.style.backgroundColor = color;
 element = document.getElementById("ele1");
 element.style.backgroundColor = 'green';
 element.addEventListener("click", function(){pintar(element,'yellow')});
+
+
+/*=======================================================================
+  ======================== FUNCION 4_COLORES.HTML =======================
+  =======================================================================*/
+
+  document.addEventListener('keydown', function (event) {
+    if (event.key === 'a') {
+    /* Cambiar a color 1 */
+    } else if (event.key === 's') {
+    /* Cambiar a color 2 */
+    }
+    })
